8cca80a9846d0ce78e82e71822d5bc6138f9a0a3
Renata <r205320@dac.unicamp.br>
2019-10-09 21:57:22 -0300

    ,

--- padroesDeProjeto1/src/padroesDeProjeto1/ActionHandler.java
+++ padroesDeProjeto1/src/padroesDeProjeto1/ActionHandler.java
@@ -3,14 +3,6 @@ package padroesDeProjeto1;
 public class ActionHandler extends Observador {
 	private int estadoObservador;
 	
-	public void update(int nivel) {
-		if (s.temperatura >= 360) {
-			FabricaAcoesEmail.criarAcaoAlerta();	
-		} 
-		else if (s.temperatura >= 180) {
-			FabricaAcoesEmail.criarAcaoPerigo();
-		}
-		
-	}
+	
 	
 }
